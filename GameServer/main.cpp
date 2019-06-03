fa3d6acc8dfd6451c4d1067d4586ae2bd95bc652
hourui <47287984+liquidmonkeyCH@users.noreply.github.com>
2019-06-03 16:40:24 +0800

    Update main.cpp

--- GameServer/main.cpp
+++ GameServer/main.cpp
@@ -7,43 +7,20 @@
 
 using namespace Utility;
 
-class test
-{
-public:
-	static int* instance(void)
-	{
-		static int num;
-		return &num;
-	}
-};
-
 int main(int argc, char* argv[])
 {
 	logger klogger(logger::log_level::debug);
 	Clog::active_logger(&klogger);
 
-	Clog::info("%p", test::instance());
-
-#ifndef _WIN32
-	int pid = fork();
-	if (pid != 0)
-	{
-		// parent exit
-		exit(-1);
-	}
-	// create a new session
-#endif // !_WIN32
-
-	Clog::info("%p", test::instance());
-	/*
 	CSingleton<main::GameServer>* server = CSingleton<main::GameServer>::GetInstance();
 
 	if (!server->Start(true, "game_server",argc,argv))
 		return 0;
 
 	server->Run();
-	*/
+#ifdef _WIN32
 	system("pause");
+#endif
     return 0;
 }
 
